a4c8285c5d75d767351e4c889bddb56bb2e94839
Hitesh Patel <hbp976@gmail.com>
2020-10-19 09:37:00 -0600

    second commit

--- CsharpReview_4.2/Program.cs
+++ CsharpReview_4.2/Program.cs
@@ -6,7 +6,7 @@ namespace CsharpReview_4._2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!what a nice day!");
         }
     }
 }
